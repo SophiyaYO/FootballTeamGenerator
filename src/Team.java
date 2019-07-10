862d8d3a866e57e7a518b517aa3ca93f809d65b6
SophYo <sophiya.dimitrova@gmail.com>
2019-07-10 22:17:51 +0300

    adding fields, constructor, get/setName(), addPlayer()- with no functionality yet , removePlayer() -with no functionality yet

--- src/Team.java
+++ src/Team.java
@@ -11,6 +11,29 @@ public class Team {
     }
 
     private void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            validateName();
+        }
+        this.name = name;
+    }
+
+    private void validateName() {
+        throw new IllegalArgumentException("A name should not be empty.");
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void addPlayer(Player player) {
 
     }
+
+    public void removePlayer(String removePlayer) {
+        for (Player player : this.players) {
+//            if (player)
+        }
+    }
+
+
 }
