9c3ec6618f0821130f0a34dde027231ee0b63e10
SophYo <sophiya.dimitrova@gmail.com>
2019-07-10 21:42:51 +0300

    initial Commit with classes in src

---
+++ src/Team.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Team {
+    private String name;
+    private List<Player> players;
+
+    public Team(String name) {
+        this.setName(name);
+        this.players = new ArrayList<>();
+    }
+
+    private void setName(String name) {
+
+    }
+}
